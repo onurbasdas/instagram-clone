fb0858a5a3ba4263f859170d6cebf17b4c2f2424
onurbasdas <onurebasdase@hotmail.com>
2021-01-27 21:32:25 +0300

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Onur Başdaş on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
